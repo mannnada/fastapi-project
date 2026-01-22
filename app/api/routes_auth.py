3729b3886454e57248a013d89b484543767f0765
mannnada123 <mannnada05@gmail.com>
2026-01-21 20:08:00 -0800

    added authorization

---
+++ app/api/routes_auth.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from app.core.security import create_token
+
+router = APIRouter()
+
+class AuthInput(BaseModel):
+    username: str
+    password: str
+    
+@router.post('/login')
+def login(auth: AuthInput):
+    if (auth.username == 'admin') and (auth.password == 'admin'):
+        token = create_token(data={'sub': auth.username})
+        return {'access_token': token}
+    return {'error': 'Invalid Credentials'}
+
